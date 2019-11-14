b9848bacfcaefc042212e6ba5f32a8f04ae9b947
Madeline Corman <madelinecorman@gmail.com>
2019-11-14 16:08:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 # require modules here
 require "yaml"
 
-def load_library(emoticons)
-  emoticons = YAML.load_file('emoticons.yml')
+def load_library("./lib/emoticons.yml")
+  emoticons = YAML.load_file("emoticons.yml")
 end
 
 def get_japanese_emoticon
